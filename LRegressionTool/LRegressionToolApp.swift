cf6753ce0dd21cef308ffd2fe6ec622eca968158
Yegor Kolobaev <lroofer@icloud.com>
2023-08-12 17:44:47 +0300

    Initial Commit

---
+++ LRegressionTool/LRegressionToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  LRegressionToolApp.swift
+//  LRegressionTool
+//
+//  Created by Егор Колобаев on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct LRegressionToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
